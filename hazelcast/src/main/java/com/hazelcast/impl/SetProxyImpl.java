cdef9b798380ab0bcce9198af9d6b5e58934c2f0
Mehmet Dogan <mehmet@hazelcast.com>
2012-06-14 12:55:24 +0300

    Fixes #189. (cherry picked from commit fe1a00119a6570fef6159d990e9d556d1d14105d)

--- hazelcast/src/main/java/com/hazelcast/impl/SetProxyImpl.java
+++ hazelcast/src/main/java/com/hazelcast/impl/SetProxyImpl.java
@@ -153,6 +153,11 @@ public class SetProxyImpl extends AbstractCollection implements SetProxy, DataSe
         return base.getMProxy();
     }
 
+    public boolean removeKey(final Object key) {
+        ensure();
+        return base.removeKey(key);
+    }
+
     class SetProxyReal extends AbstractCollection implements SetProxy {
 
         final MProxy mapProxy;
@@ -230,7 +235,12 @@ public class SetProxyImpl extends AbstractCollection implements SetProxy, DataSe
             return mapProxy;
         }
 
+        public boolean removeKey(final Object key) {
+            return mapProxy.removeKey(key);
+        }
+
         public void setHazelcastInstance(HazelcastInstance hazelcastInstance) {
         }
+
     }
 }
